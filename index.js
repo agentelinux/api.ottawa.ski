cd5439ce60e70a7b9cddfd178afbc5b0cc6a067a
Jason Mulligan <jason.mulligan@avoidwork.com>
2016-07-07 14:36:56 -0400

    sigh

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const tenso = require('tenso'),
     path = require('path'),
     merge = require('tiny-merge'),
-    config = require(path.join(__dirname, 'src', 'config.json')),
+    config = require(path.join(__dirname, 'config.json')),
     routes = require(path.join(__dirname, 'src', 'routes.js'));
 
 let app = tenso(merge({routes: routes}, config));
