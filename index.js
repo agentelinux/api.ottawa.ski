735ab411610a96c454762efd8fcd490bf3d4291f
Jason Mulligan <jason.mulligan@avoidwork.com>
2016-07-08 15:02:00 -0400

    Updating CORS config

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const tenso = require('tenso'),
     config = require(path.join(__dirname, 'config.json')),
     routes = require(path.join(__dirname, 'src', 'routes.js'));
 
-let app = tenso(merge({routes: routes}, config));
+tenso(merge({routes: routes}, config));
